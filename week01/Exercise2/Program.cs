473c613d4c2e93647e9ff40de39148d3aeb55d2d
JoyceChizondo <joymug14@gmail.com>
2026-09-04 15:33:51 +0200

    Execise 2 done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,76 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letter = "";
+
+        if (grade >=90)
+        {   
+            letter = "A";
+        }
+        else if (grade >=80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter ="C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        int lastDigit = grade % 10;
+
+        string sign = "";
+
+        if (lastDigit >= 7)
+        {
+            sign ="+";
+
+        }
+
+        else if (lastDigit <=3)
+        {
+            sign = "-";
+        }
+
+        
+        if (letter == "A" && sign =="+" )
+        {
+            sign = "";
+        }
+        if (letter == "F")
+        {
+            sign ="";
+        }
+
+
+        Console.WriteLine($"Your grade is : {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations. You Passed!");
+
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
+        }
+
+
     }
 }
