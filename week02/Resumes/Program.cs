6856df622e0e348a0356892fdb4ea91a9bc0c97d
JoyceChizondo <joymug14@gmail.com>
2026-09-10 22:58:05 +0200

    Activity completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
-    }
+        Job job1 = new Job();
+        job1._jobTitle = "Auto Electrian";
+        job1._company = "ZUPCO";
+        job1._startYear = 2021;
+        job1. _endYear =2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Service Technician";
+        job2._company = "Marce";
+        job2._startYear = 2024;
+        job2. _endYear =2025;
+
+        Resume myResume = new Resume();
+        myResume._name ="Joyce chzondo";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    }   
 }
